442bffcd4e54da8dde4a2950009c5cdbf049e04b
Oisin33 <oisinmcsherry2@gmail.com>
2021-12-03 23:49:35 +0000

    Added checks to RegistrationView that will check if the email is valid as well as multiple tests on the password such as checking the length, if it has a letter, number and symbol

--- Registration/src/main/java/com/example/registration/RegistrationView.java
+++ Registration/src/main/java/com/example/registration/RegistrationView.java
@@ -7,6 +7,12 @@ public class RegistrationView {
     private String email;
     private String password;
 
+    private static final Pattern letterPatterns = Pattern.compile("[a-zA-Z]");
+    private static final Pattern numericPatterns = Pattern.compile("[\\d]");
+    private static final Pattern symbolPatterns = Pattern.compile("[*^&@!]");
+    private static final Pattern emailPatterns = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+
+    private static Matcher match;
     public RegistrationView(String email, String password) {
         this.email = email;
         this.password = password;
@@ -21,8 +27,28 @@ public class RegistrationView {
         return this.email;
     }
 
+    public boolean validEmail() {
+        return emailPatterns.matcher(this.email).find();
+    }
+
     public String getPassword() {
         return this.password;
+    }
+
+    public boolean validLength() {
+        return this.password.length() > 6;
+    }
+
+    public boolean hasLetter() {
+        return letterPatterns.matcher(this.password).find();
+    }
+
+    public boolean hasNumber() {
+        return numericPatterns.matcher(this.password).find();
+    }
+
+    public boolean hasSymbol() {
+        return symbolPatterns.matcher(this.password).find();
     }
 
 }
